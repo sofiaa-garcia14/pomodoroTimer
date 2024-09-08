59aff95b858dca0b2afe53f2f711f721631301d8
sofiaa-garcia14 <sofiaamaliagarcia@gmail.com>
2024-09-08 14:23:25 -0300

    Add footer in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Footer } from "@/components/ui/footer";
 import PomodoroTimer from "@/components/ui/PomodoroTimer";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-4">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-4 gap-y-20">
       <PomodoroTimer />
+      <Footer />
     </main>
   );
 }
